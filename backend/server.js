1167ac9f67fa3528b938afac0b8ffd0edd9d1e4c
Ayush Bhongade <your-email@example.com>
2026-08-29 23:48:30 +0530

    update backend cors for production

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:4173",
+  "https://food-app-xi-rust.vercel.app"
 ];
 
 app.use(express.json());
